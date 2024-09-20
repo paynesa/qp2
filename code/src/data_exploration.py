eec9bfa6a9c434219b65edfd30ab38810346dbe1
paynesa <sarah.payne@stonybrook.edu>
2024-09-20 14:57:59 -0400

    finish up exploration_plotting notebook

--- code/src/data_exploration.py
+++ code/src/data_exploration.py
@@ -1,4 +1,8 @@
-import numpy as np 
+import os
+import matplotlib.pyplot as plt
+from scipy import stats 
+import numpy as np
+
  
 def parse_files(path, element=-1):
     """Helper function to get the train, dev, and test elements for a given path"""
@@ -119,4 +123,46 @@ def overlap_less(second, ovlp):
             ovlp_new.append(o)
             second_new.append(s) 
     return second_new, ovlp_new
+
+
+def plot_stats(x, y, xlab, ylab, savefig=None, less=False):
+    """Helper function to make the plots aesthetic and run stats"""
+    
+    # Convert the input to arrays so we can get a line of best fit 
+    if less:
+        x, y = overlap_less(x, y)
+    x = np.asarray(x)
+    y = np.asarray(y)
+    a, b = np.polyfit(x, y, 1)
+    
+    # Plot the points and the line of best fit 
+    plt.clf()
+    plt.scatter(x, y, color="indigo", alpha=0.2, s=70)
+    plt.plot(x, a*x+b, linestyle="-", color="indigo", linewidth=1)
+    plt.xlabel(xlab)
+    plt.ylabel(ylab)
+    plt.title(f"{ylab} vs. {xlab}")
+    if savefig is None:
+        plt.show()
+    else:
+        plt.savefig(savefig, dpi=500)
+    
+    # Run the statistics 
+    pearson = stats.pearsonr(x, y)
+    spearman = stats.spearmanr(x, y)
+    kendall_tau = stats.kendalltau(x, y)
+    for name, stat in [("Pearson's R", pearson), ("Spearman", spearman), ("Kendall's Tau", kendall_tau)]:
+        print(f"{name}:\t{stat.statistic :.3f},\t p = {stat.pvalue}")
+
+def run_correlation(PATH_TO_DATA, funct1, funct2):
+    """Run a single correlation between two functions over the data"""
+    xs = []
+    ys = []
+    langs = []
+    for language_family in [l for l in os.listdir(PATH_TO_DATA) if "." not in l]:
+        for language in set([f.split(".")[0] for f in os.listdir(f"{PATH_TO_DATA}/{language_family}")]):
+            xs.append(funct1(f"{PATH_TO_DATA}/{language_family}/{language}"))
+            ys.append(funct2(f"{PATH_TO_DATA}/{language_family}/{language}"))
+            langs.append(f"{language_family}/{language}")
+    return xs, ys, langs
 
